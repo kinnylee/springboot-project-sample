a5650d0bae7a282186537101ecc304bee87bb167
lijl-c <lijl-c@glodon.com>
2018-10-22 20:43:11 +0800

    修改模块名称

--- springboot-project-dao-config/src/main/java/com/kinnylee/config/MybatisConfig.java
+++ springboot-project-dao-config/src/main/java/com/kinnylee/config/MybatisConfig.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -15,7 +16,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 /**
- *
+ * 数据源作为多模块的子模块独立时，yml格式配置文件，需要手动配置数据源
  * @author lijl-c
  * @date 2018/10/16
  */
@@ -24,9 +25,12 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 @MapperScan("com.kinnylee.dao")
 @PropertySource(value = "classpath:mybatis.properties", ignoreResourceNotFound = true)
 public class MybatisConfig {
-  private String mapperLocationPattern = "classpath:com/kinnylee/mapper/**/*.xml";
 
-  private String configLocation = "classpath:MybatisAppConfig.xml";
+  @Value("${mybatis.mapper-locations}")
+  private String mapperLocationPattern;
+
+  @Value("${mybatis.config-location}")
+  private String configLocation;
 
 
   @Bean("dataSource")
